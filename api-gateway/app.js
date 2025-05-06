bde0bae72997644429b5cc1d5ea98e5d420d0cb5
Yamnyr <quentinwaroquet@gmail.com>
2025-05-06 11:42:08 +0200

    first commit

---
+++ api-gateway/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+
+// Redirection vers le User Service
+app.use('/api/users', createProxyMiddleware({
+    target: 'http://localhost:4000',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api/users': '/users',
+    },
+}));
+
+// Redirection vers le postService
+app.use('/api/posts', createProxyMiddleware({
+    target: 'http://localhost:5000',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api/products': '/posts',
+    },
+}));
+
+const PORT = process.env.GATEWAY_PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`API Gateway démarrée sur le port ${PORT}`);
+});
